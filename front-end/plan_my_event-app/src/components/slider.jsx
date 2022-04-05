ad0a04006345f99688c74d41dd0aeab91cead6e3
sai_chaitanya <sai.chaitanya@siply.in>
2022-04-06 02:39:08 +0530

    slider - pending

---
+++ front-end/plan_my_event-app/src/components/slider.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import styled from 'styled-components'
+import {ArrowLeftOutlined, ArrowRightOutlined} from '@material-ui/icons'
+import pic from "../Images/marriage_image_4.jpg"
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+position: relative;
+overflow: hidden;
+`;
+
+const Wrapper = styled.div`
+height: 100%;
+display: flex;
+`;
+const Slide = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+align-items: center;
+`;
+
+const ImgContainer = styled.div`
+// height: 100%;
+flex: 1;`;
+
+const Image = styled.img`
+width : 100%`;
+
+const InfoContainer = styled.div`
+flex: 1;
+padding 50px;
+`;
+
+const Title = styled.h1``;
+const Description = styled.p`
+margin: 5-px 0px;
+font-size: 20px;
+font-weight: 500;
+letter-spacing: 3px;
+`;
+const Button = styled.button`
+padding: 10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+`;
+
+const Arrow = styled.div`
+width: 50px;
+height: 50px;
+background-color: grey;
+border-radius: 50%;
+display: flex;
+align-items: center;
+justify-content: center;
+position: absolute;
+top: 0;
+bottom: 0;
+left :${props=> props.direction === "left" && "10px"};
+right : ${props=> props.direction === "right" && "10px"};
+margin: auto;
+cursor: pointer;
+opacity : 0.5;
+`;
+
+const Slider = () => {
+
+    const handleClick = (direction)
+  return (
+    <Container>
+        <Arrow direction="left" onClick={()=>handleClick("left")}>
+            <ArrowLeftOutlined/>
+        </Arrow>
+        <Wrapper>
+            <Slide>
+            <ImgContainer>
+                <Image src={pic}/>
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Marriage</Title>
+                <Description>Where there is love there is life.</Description>
+                <Button>Learn More</Button>
+            </InfoContainer>
+            </Slide>
+            <Slide>
+            <ImgContainer>
+                <Image src={pic}/>
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Marriage</Title>
+                <Description>Where there is love there is life.</Description>
+                <Button>Learn More</Button>
+            </InfoContainer>
+            </Slide>
+            <Slide>
+            <ImgContainer>
+                <Image src={pic}/>
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Marriage</Title>
+                <Description>Where there is love there is life.</Description>
+                <Button>Learn More</Button>
+            </InfoContainer>
+            </Slide>
+        </Wrapper>
+        <Arrow direction="right" onClick={()=>handleClick("right")}>
+            <ArrowRightOutlined/>
+        </Arrow>
+    </Container>
+  )
+}
+
+export default Slider

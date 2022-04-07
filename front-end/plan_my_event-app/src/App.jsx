07296383356bf7cb1cf6c82f0d9fca989ad68467
sai_chaitanya <sai.chaitanya@siply.in>
2022-04-07 15:22:01 +0530

    Login and Register Pages - complete

--- front-end/plan_my_event-app/src/App.jsx
+++ front-end/plan_my_event-app/src/App.jsx
@@ -1,13 +1,15 @@
 //import logo from './logo.svg';
 //import './App.css';
 import Home from "./pages/Home";
+import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 function App() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  );
+    return (
+        <div>
+            <Home/>
+        </div>
+    );
 }
 
 export default App;

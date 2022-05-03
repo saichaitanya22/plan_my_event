d5e1a88185b00f2d66a95789ef653ddf19a3e357
Ritika Garg <ritika.g20@gmail.com>
2022-05-03 08:08:04 +0530

    Add Product List and Modify Home.jsx

--- front-end/plan_my_event-app/src/pages/Home.jsx
+++ front-end/plan_my_event-app/src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Navbar from "../components/NavBar"
+import React from "react";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+import Products from "../components/Products";
 import Slider from "../components/Slider";
 
 const Home = () => {
     return (
         <div>
-            <Navbar/>
-            <Slider/>
+            <NavBar />
+            <Slider />
+            <Categories />
+            <Products/>
+            <Footer/>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
